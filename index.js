42190e2905f07dd1d1803297cdf581b74d779fd2
derpierre65 <git@derpierre65.de>
2024-09-17 13:34:28 +0100

    chore: add more debug

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 try {
+  const token = core.getInput('GITHUB_TOKEN');
+  if (!token) {
+    core.setFailed('GITHUB_TOKEN is required');
+    return;
+  }
+
   // /([A-Za-z]{2,4}-\d+)/g
 
   console.log('urls', core.getInput('urls'));
   console.log(process.env.GITHUB_TOKEN);
+  console.log(JSON.stringify(github.context));
+
+  console.log(github.getOctokit(token));
 }
 catch (error) {
   core.setFailed(error.message);
