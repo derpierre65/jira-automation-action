0d003472b6c7ac813f529082527c3934f0202d0c
derpierre65 <git@derpierre65.de>
2024-09-17 13:00:04 +0100

    chore: add debug

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import * as core from '@actions/core';
 try {
   // /([A-Za-z]{2,4}-\d+)/g
 
-  console.log('test');
+  console.log('test', [
+    core.getInput('status'),
+    core.getInput('urls'),
+  ]);
 }
 catch (error) {
   core.setFailed(error.message);
