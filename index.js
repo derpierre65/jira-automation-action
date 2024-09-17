3f777a4c6172c99abb2eafd16598ef190351bbea
derpierre65 <git@derpierre65.de>
2024-09-17 13:53:59 +0100

    feat: fetch all commit messages and all issue ids in commits

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+async function fetchCommitMessages(octokit) {
+  const commitMessages = [];
+  let hasMoreCommits = true;
+  let perPage = 100;
+  let page = 1;
+
+  while (hasMoreCommits) {
+    core.info(`Fetching commits page ${page}`);
+
+    const { data: commits } = await octokit.request(`GET ${context.payload.pull_request.commits_url}`, {
+      page,
+      per_page: perPage,
+    });
+
+    commits.forEach(commit => commitMessages.push(commit.commit.message));
+
+    hasMoreCommits = commits.length === perPage;
+    page++;
+  }
+
+  return commitMessages;
+}
+
+function getIssueIds(messages) {
+  const issueIds = [];
+  for ( const message of messages ) {
+    issueIds.push(...message.match(/([A-Za-z]{2,4}-\d+)/g) || []);
+  }
+
+  return [...new Set(issueIds)];
+}
+
 async function run() {
   try {
     const token = core.getInput('GITHUB_TOKEN');
@@ -11,32 +43,15 @@ async function run() {
 
     const context = github.context;
 
-    // /([A-Za-z]{2,4}-\d+)/g
-
     console.log('urls', core.getInput('urls'));
-    console.log(JSON.stringify(github.context));
+    console.log(JSON.stringify(github.context, null, 4));
 
     const octokit = github.getOctokit(token);
-
-    const commitMessages = [];
-    let perPage = 5;
-    let hasMoreCommits = true;
-    let page = 1;
-    while (hasMoreCommits) {
-      core.info(`Fetching commits page ${page}`);
-
-      const { data: commits } = await octokit.request(`GET ${context.payload.pull_request.commits_url}`, {
-        page,
-        per_page: perPage,
-      });
-
-      commits.forEach(commit => commitMessages.push(commit.commit.message));
-
-      hasMoreCommits = commits.length === perPage;
-      page++;
-    }
+    const commitMessages = await fetchCommitMessages(octokit);
+    const issueIds = getIssueIds(commitMessages);
 
     core.info(JSON.stringify(commitMessages));
+    core.info(JSON.stringify(issueIds));
   }
   catch (error) {
     core.setFailed(error.message);
