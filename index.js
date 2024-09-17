4e3eb43b4a5f1821e930eb1b4c38de791e339945
derpierre65 <git@derpierre65.de>
2024-09-17 13:24:15 +0100

    chore: remove status input

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ import * as core from '@actions/core';
 try {
   // /([A-Za-z]{2,4}-\d+)/g
 
-  console.log('test', [
-    core.getInput('status'),
-    core.getInput('urls'),
-  ]);
+  console.log('urls', core.getInput('urls'));
 }
 catch (error) {
   core.setFailed(error.message);
